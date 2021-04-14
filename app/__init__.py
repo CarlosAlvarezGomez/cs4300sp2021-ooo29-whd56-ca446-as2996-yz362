b4796e54c4fbfd154871a920e3ab271a30a2170c
wemi <>
2021-04-14 20:41:00 +0100

    super basic system

--- app/__init__.py
+++ app/__init__.py
@@ -29,4 +29,5 @@ socketio.init_app(app)
 # HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
-  return render_template("404.html"), 404
+  return render_template("explain.html"), 404
+  # return render_template("404.html"), 404
